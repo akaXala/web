6ce68f716f4168aae14d0af5ac328d5210a576c9
akaXala <144200243+akaXala@users.noreply.github.com>
2024-06-21 15:59:25 -0600

    Actualización cartas
    Ya se muestran todos los items con su respectiva carta

--- js/index.js
+++ js/index.js
@@ -1,19 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     let objProductos = JSON.parse(productos);
 
+    let filas = "";
+    let col = 0;
+
     objProductos.forEach((objeto, indice)=>{
-    carta = `
-        <div class="card" style="width: 18rem;">
-            <img src="${objeto.thumbnail}" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">${objeto.title}</h5>
-                <p class="card-text">${objeto.description}</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+        if (indice % 3 == 0){
+            filas += `<div class="container text-center">
+                        <div class="row align-items-start">`;
+        }
+
+        filas += `
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="${objeto.thumbnail}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${objeto.title}</h5>
+                        <p class="card-text">${objeto.description}</p>
+                        <a href="#" class="btn btn-primary">Ir</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    `;
+        `;
+
+        if (col == 2){
+            filas += `
+                    </div>
+                </div>
+            `;
+            col = 0;
+        } else {
+            col++;
+        }
     });
 
     let verProductos = document.querySelector("div#verProductos");
-    verProductos.innerHTML = carta;
+    verProductos.innerHTML = filas;
 });
