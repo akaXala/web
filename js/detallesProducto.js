1bbf27fb7bfbe062573d0fbc8a647ec8f5248636
akaXala <144200243+akaXala@users.noreply.github.com>
2024-06-22 16:26:27 -0600

    Implementación carrousel para el HTML dinamico, implementación JS de Bootstrap

--- js/detallesProducto.js
+++ js/detallesProducto.js
@@ -16,11 +16,49 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function displayProductDetails(product) {
+    let imagesHTML = '';
+
+    if (product.images.length > 1) {
+        imagesHTML += `
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+        `;
+
+        for (let i = 0; i < product.images.length; i++) {
+            if (i == 0) {
+                imagesHTML += `
+                <div class="carousel-item active">
+                    <img src="${product.images[i].trim()}" class="d-block w-100">
+                </div>`;
+            } else {
+                imagesHTML += `
+                <div class="carousel-item">
+                    <img src="${product.images[i].trim()}" class="d-block w-100">
+                </div>`;
+            }
+        }
+
+        imagesHTML += `
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        `;
+    } else {
+        imagesHTML += `<img src="${product.images[0]}" class="img-fluid border">`;
+    }
+
     const productDetails = `
         <div class="container text-center">
             <div class="row">
                 <div class="col">
-                    <img src="${product.images}" class="img-fluid border">
+                    ${imagesHTML}
                 </div>
                 <div class="col">
                     <div class="row">
@@ -28,6 +66,9 @@ function displayProductDetails(product) {
                     </div>
                     <div class="row">
                         <p>${product.description}</p>
+                    </div>
+                    <div class="row">
+                        <p>${product.price}</p>
                     </div>
                 </div>
             </div>
